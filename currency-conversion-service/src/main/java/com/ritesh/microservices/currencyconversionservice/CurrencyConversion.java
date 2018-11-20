c871ae49d2c21c6dadd9d5bceb4e3748165a0fae
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2018-11-20 17:31:02 +0530

    first commit

---
+++ currency-conversion-service/src/main/java/com/ritesh/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,98 @@
+package com.ritesh.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+	
+	private Long id;
+	
+	private String from;
+	
+	private String to;
+	
+	private BigDecimal conversionMultiple;
+	
+	private BigDecimal quantity;
+	
+	private BigDecimal totalCalculatedAmount;
+	
+	private int port;
+
+	public CurrencyConversion() {
+		
+	}
+	
+	
+	public CurrencyConversion(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity,
+			BigDecimal totalCalculatedAmount, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.port = port;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public BigDecimal getQunatity() {
+		return quantity;
+	}
+
+	public void setQunatity(BigDecimal qunatity) {
+		this.quantity = qunatity;
+	}
+
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
+	
+
+}
---
+++ currency-conversion-service/src/main/java/com/ritesh/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,54 @@
+package com.ritesh.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+
+	/*
+	 * @Autowired private Environment env;
+	 */
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion convertCurrency(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		Map<String,String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class,uriVariables);
+		CurrencyConversion response = responseEntity.getBody();
+		logger.info("inside convert currency {} :>>>>>>>>>>>>>>");
+
+		return new CurrencyConversion(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+
+	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		CurrencyConversion response = proxy.retreiveExchangeValue(from, to);
+		logger.info("inside convert currency {} using feign :>>>>>>>>>>>>>>");
+
+		return new CurrencyConversion(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+
+	}
+
+}
